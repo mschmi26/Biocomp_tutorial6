0911faf1f915c9a5b88180b8bab7a6719f02c76b
Megan Schmidt <mschmi26@nd.edu>
2021-10-07 23:44:53 -0400

    Question2

--- question2.sh
+++ question2.sh
@@ -1,5 +1,5 @@
 #Returns the top earner in a company, the lowest earner in the company, and 
-#the number of females in the top ten earning positions in the company
+#the number of females in the top ten earning wage positions in the company
 #Usage bash question2.sh file.csv
 
 variable=$(cat $1 | cut -d "," -f 1,2,4 | grep "gender" -v | sed 's/,/ /' | sed 's/,/ /' | sort -V -r -k 3 | head -n 1)
